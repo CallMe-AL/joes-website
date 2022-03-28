792ee8330e45ab5f2fce6519cd9b851cd662dcad
unknown <aindovina2@yahoo.com>
2022-03-28 14:10:34 -0400

    updated baseurl

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,33 @@
 const upBtn = document.querySelector('.back-to-top-btn');
+
+const hamMenu = document.querySelector('.menu-container');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuOverlay = document.querySelector('.menu-overlay');
+
+hamMenu.addEventListener('click', () => {
+  if (!hamMenu.classList.contains('close')) {
+    hamMenu.classList.add('close');
+    mobileMenu.classList.add('active');
+    menuOverlay.classList.add('active');
+  } else {
+    hamMenu.classList.remove('close');
+    mobileMenu.classList.remove('active');
+    menuOverlay.classList.remove('active');
+  }
+});
+
+menuOverlay.addEventListener('click', () => {
+  if (!hamMenu.classList.contains('close')) {
+    hamMenu.classList.add('close');
+    mobileMenu.classList.add('active');
+    menuOverlay.classList.add('active');
+  } else {
+    hamMenu.classList.remove('close');
+    mobileMenu.classList.remove('active');
+    menuOverlay.classList.remove('active');
+  }
+});
+
 window.addEventListener('scroll', () => {
   if (window.pageYOffset > 500) {
     upBtn.classList.add('show');
